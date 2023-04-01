ad02db7c5b6bf1ed37475d7665d21e0f7ba47955
ztrkosmn09 <ztrkosmn09@gmail.com>
2023-04-01 12:58:29 +0200

    nextjs und tailwindcss wurden installiert

---
+++ pages/home/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+       <div className="text-red-600 font-dancing text-center text-3xl kaybol">Feane</div>
+    </div>
+  )
+}
+
+export default Home
